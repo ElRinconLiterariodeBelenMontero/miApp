6c31893996e766bbbfd8d8fb69fa33f1344f7281
HP <HP@FRAN>
2024-11-09 12:48:26 +0100

    Carga Inicial

---
+++ src/main/java/com/example/miapp/controller/MiAppController.java
@@ -0,0 +1,17 @@
+package com.example.miapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MiAppController {
+
+    // Endpoint para obtener un mensaje simple
+    @GetMapping("/mensaje")
+    public String obtenerMensaje() {
+        return "¡Hola desde el controlador de Spring Boot!";
+    }
+}
+
